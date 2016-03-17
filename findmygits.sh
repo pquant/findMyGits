00573ba350e3f93c8f00af9c4dcbfd9af566ac05
pquant <pquant@hotmail.fr>
2016-03-17 14:45:29 +0000

    Missing changes in bash script

--- findmygits.sh
+++ findmygits.sh
@@ -1,6 +1,6 @@
 #!usr/bin/bash
 
-FUNC_REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/findmygits"
+FUNC_REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/src"
 cd $FUNC_REPO
 #echo $FUNC_REPO
 . bin/activate
