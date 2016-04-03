2172d7447cc50c01d4ee4f6ce4920a4475d40495
pquant <pquant@hotmail.fr>
2016-04-03 15:41:21 +0100

    Added --include-only flag to search one directory only

--- src/findmygits.py
+++ src/findmygits.py
@@ -3,15 +3,13 @@ import subprocess
 import argparse
 from pprint import pprint
 
-def find_repos(exclude_dirs=None):
+
+def find_repos(exclude_dirs=None, only_dirs=None):
+    # Check flag consistency
+    _check_flag_consistency(exclude_dirs, only_dirs)
     # Define shell command to run
-    home = os.environ['HOME']
-    shell_find_cmd = 'find ' + home + ' -type d -name "*.git"'
-    # Add directories to exclude from search, if specified by user
-    if exclude_dirs is not None:
-        for d in exclude_dirs:
-            shell_find_cmd += '| grep -v ' + d
-    # Integrate shell command in a subprocess and run
+    shell_find_cmd = _write_cmd(exclude_dirs, only_dirs)
+    # Integrate shell command in a subprocess and run it with shell
     p = subprocess.Popen(shell_find_cmd, shell=True, stdout=subprocess.PIPE)
     # Retrieve output
     repo_list = _decode_process(p).split('\n')[:-1]
@@ -35,7 +33,7 @@ def print_repos(bare_repos, active_repos):
     format_string = small_bar + '\n{0}:\n' + small_bar + '\nREMOTES:\n{1}'
 
     print(bar)
-    print("-- Bare repos (expected to follow the naming convention XXX.git and further checked using 'git rev-parse --is_bare_repository')")
+    print("-- Bare repos (determined using 'git rev-parse --is_bare_repository')")
     print(bar)
     for r in bare_repos:
         print(r)
@@ -77,18 +75,46 @@ def _is_bare_repo_cmd(repo):
     is_bare_repo = _decode_process(p).split('\n')[0].upper() == 'TRUE'
     return is_bare_repo
 
+
+def _check_flag_consistency(exclude_dirs, only_dirs):
+    both_not_none = exclude_dirs is not None and only_dirs is not None
+    if both_not_none:
+        raise SystemExit('One of ' + _flags['exclude'] + ' or ' + _flags['include-only'] + ' flags expected, not both')
+    if exclude_dirs is []:
+        raise SystemExit(_flags['exclude'] + ' flag provided without directories to exclude')
+
+def _write_cmd(exclude_dirs, only_dirs):
+    home = os.environ['HOME']
+    shell_find_cmd = ''
+    # Restrict to dome dirs only
+    if only_dirs is not None:
+        for d in only_dirs:
+            shell_find_cmd = 'find ' + d + ' -type d -name "*.git"'
+    # Add directories to exclude from search, if specified by user
+    elif exclude_dirs is not None:
+        shell_find_cmd = 'find ' + home + ' -type d -name "*.git"'
+        for d in exclude_dirs:
+            shell_find_cmd += '| grep -v ' + d
+    else:
+        shell_find_cmd = 'find ' + home + ' -type d -name "*.git"'
+
+    return shell_find_cmd
+
+
+_flags = { 'exclude': '--exclude-dirs',
+           'include-only': '--include-only'}
 ########################################################
 # Main
 ########################################################
 if __name__ == '__main__':
     # Define ParserExtract args from command-line
     parser = argparse.ArgumentParser('Lists all local git repos on your computer. Built on Ubuntu 15.10 (Wily)')
-    parser.add_argument('--exclude', nargs='*', dest='exclude', metavar='dir',  help='Directories to exclude')
-    exclude_arg = parser.parse_args()
-    exclude_dirs = exclude_arg.exclude
-    if exclude_dirs is []:
-        raise SystemExit('--exclude flag provided without directories to exclude')
+    parser.add_argument(_flags['exclude'], nargs='*', dest='exclude', metavar='dir',  help='Directories to exclude')
+    parser.add_argument(_flags['include-only'], nargs=1, dest='include_only', metavar='dir',  help='One Directory to include exclusively')
+    args = parser.parse_args()
+    exclude_dirs = args.exclude
+    include_only_dir = args.include_only
     # Get all active and bare repos
-    active_repos, bare_repos = find_repos(exclude_dirs)
+    active_repos, bare_repos = find_repos(exclude_dirs, include_only_dir)
     # print output
     print_repos(bare_repos, active_repos)
